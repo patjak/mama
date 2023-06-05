34f2df8883ca4afa635fdbf9e7756e727954dd76
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2023-06-05 14:25:24 +0200

    Add the boot.php file used by www server for serving ipxe scripts
    Signed-off-by: Patrik Jakobsson <pjakobsson@suse.de>

---
+++ src/boot.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_GET['MAC'])) {
+	$mac = $_GET['MAC'];
+} else {
+	exec("ip -4 neigh | grep ".$client_ip, $mac);
+	$mac = explode(" ", $mac[0]);
+	if (count($mac) == 6)
+		$mac = $mac[4];
+	else
+		$mac = "";
+
+	if ($mac == "") {
+		echo "#!ipxe\n";
+		echo "echo No MAC specified";
+		echo "shell";
+		exit();
+	}
+}
+
+$server_ip = $_SERVER['SERVER_ADDR'];
+$client_ip = $_SERVER['REMOTE_ADDR'];
+
+passthru("mama ipxe ".$mac." ".$client_ip." ".$server_ip);
+
+?>
